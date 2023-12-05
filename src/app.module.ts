9f9bf5778bc8407fe749adf67b22bb4f648b1321
minseoking <lms98923@naver.com>
2023-12-05 14:42:09 +0900

    graphql 초기 세팅 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { typeORMConfig } from './config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
 import * as process from 'process';
 import { validate } from './config/env.validation';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
 	imports: [
@@ -12,6 +15,11 @@ import { validate } from './config/env.validation';
 			isGlobal: true,
 			envFilePath: `${process.cwd()}/src/config/env/.env.${process.env.NODE_ENV}`,
 			validate
+		}),
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			playground: true,
+			autoSchemaFile: join(process.cwd(),'/src/schema.gql')
 		}),
 		TypeOrmModule.forRootAsync(typeORMConfig),
 		UsersModule]
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,14 @@
-import { Module, } from '@nestjs/common';
-import { UsersController, } from './persistence/users.controller';
-import { UsersService, } from './application/users.service';
-import { UserRepository, } from './infrastructure/user.repository';
+import { Module } from '@nestjs/common';
+import { UsersController } from './persistence/users.controller';
+import { UsersService } from './application/users.service';
+import { UserRepository } from './infrastructure/user.repository';
+import { UserResolver } from './persistence/user.resolver';
 
 @Module({
-	controllers: [UsersController,],
-	providers: [UsersService, UserRepository,],
+	controllers: [UsersController],
+	providers: [UsersService,
+		UserRepository,
+		UserResolver]
 })
-export class UsersModule {}
+export class UsersModule {
+}
